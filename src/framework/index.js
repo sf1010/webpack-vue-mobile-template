e65a8115d7060872145995a232cdfeeaf1938512
金杭 <not.single@hotmail.com>
2019-11-15 15:37:32 +0800

    更改根组件目录

--- src/framework/index.js
+++ src/framework/index.js
@@ -1,18 +1,18 @@
 import Vue from 'vue';
 import router from '@/router';
 import store from '@/store';
-import App from './App';
-import Vant from 'vant';// https://youzan.github.io/vant/#/zh-CN/intro
+import App from '../layout/App';
+// import Vant from 'vant';// https://youzan.github.io/vant/#/zh-CN/intro
 import 'vant/lib/index.css';
-import { Icon } from 'vant';
+// import { Icon } from 'vant';
 import 'vant/lib/icon/local.css';
-import { Lazyload } from 'vant';
+// import { Lazyload } from 'vant';
 
 import '@/assets/styles/normalize.min.css';
 
-Vue.use(Vant);
-Vue.use(Icon);
-Vue.use(Lazyload);
+// Vue.use(Vant);
+// Vue.use(Icon);
+// Vue.use(Lazyload);
 
 new Vue({
   el: '#app',
